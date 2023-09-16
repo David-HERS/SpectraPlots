ac8c5597605dd74a14e50d33555fa2369d3a5bf3
David-HERS <davidhs1698@gmail.com>
2023-09-16 01:07:44 -0600

    Fix private funtions fix: change doubleunderscore to single underscore refactor:yield_keys -> yield_items no gives h5_object not h5_object.name refactor:select_keys -> select_items add:_default_function add:set_kwargs in class h5Utils  for set some keywords add:set_default for reset the default values in keywords fix:apply_keys with self.acces_h5() method

--- spectraplots/h5utils.py
+++ spectraplots/h5utils.py
@@ -32,11 +32,11 @@ class h5FileContext:
             self.h5_file.close()
 
 
-def __acces_h5(file_name_or_object, mode='r'):
-    return h5FileContext(file_name_or_object, mode=mode)
+def _acces_h5(file_name_or_object, mode='r', **kwargs):
+    return h5FileContext(file_name_or_object, mode=mode, **kwargs)
 
 
-def __all_keys(file_name_or_object, deep = 10, mode = 'r' ):
+def _all_keys(file_name_or_object, deep = 10, mode = 'r' ):
     "Recursively find all keys in an h5py.Group."
     with __acces_h5(file_name_or_object, mode = mode) as h5_object:
         keys = (h5_object.name,)
@@ -51,35 +51,39 @@ def __all_keys(file_name_or_object, deep = 10, mode = 'r' ):
     return keys
 
 
-def yield_keys(file_name_or_object, name_criteria = None,
-        object_criteria= None, deep = 10, mode = 'r' , func= None):
-    "Yields the keys for the h5 file  with some cirterias"
+def yield_items(file_name_or_object, name_criteria = None,
+        object_criteria= None, deep = 10, mode = 'r' , func= None, **kwargs):
+    """Yields the items for the h5 file  with some cirterias
+      
+    """
+    
     def __default_criteria(path):
         return True
     name_criteria = name_criteria or __default_criteria
     object_criteria = object_criteria or __default_criteria
     func = func or __default_criteria
 
-    with __acces_h5(file_name_or_object, mode = mode) as h5_object:
+    with _acces_h5(file_name_or_object, mode = mode, **kwargs) as h5_object:
         if object_criteria(h5_object) and name_criteria(h5_object.name):
             func(h5_object)
-            yield  h5_object.name
+            yield  h5_object
         deep  = abs(deep) #for bool(0jj)
         if bool(deep):
             deep -= 1
             for key, value in h5_object.items():
                 key = value.name
                 if isinstance(value, h5py.Group):
-                    yield from yield_keys(value, deep=deep, mode=mode,
+                    yield from yield_items(value, deep=deep, mode=mode,
                             name_criteria=name_criteria,
                             object_criteria=object_criteria, func=func)
                 elif (isinstance(value, h5py.Dataset) and
                         object_criteria(value) and
                         name_criteria(key)):
                     func(value)
-                    yield key
+                    yield value
 
-def __h5_keys(file_name_or_object, name_criteria = None, object_criteria = None,
+
+def _h5_keys(file_name_or_object, name_criteria = None, object_criteria = None,
         deep = 10):
     """Return names of the h5_obj with criteria name and object"""
     def __default_criteria(path):
@@ -96,8 +100,9 @@ def __h5_keys(file_name_or_object, name_criteria = None, object_criteria = None,
 ###############################################################################
 #criterias for hdf5 files
 ###############################################################################
-def __default_criteria(file_name_or_object):
+def _default_criteria(file_name_or_object):
     return True
+
 
 def is_dataset(h5_object):
     return isinstance(h5_object, h5py.Dataset)
@@ -105,6 +110,7 @@ def is_dataset(h5_object):
 
 def is_group(h5_object):
     return isinstance(h5_object, h5py.Group)
+
 
 def criteria_name(
         path, in_path=[] , not_in_path=[],
@@ -151,8 +157,11 @@ def criteria_name(
 ###############################################################################
 #for create or modify hdf5 files 
 ###############################################################################
+def _defalut_func(h5_object):
+    return None
+
+
 def string_to_float(number_string, dot='_'):
-    
     #clean number_string
     alphabet = list(string.ascii_letters)
     symbols = list(string.punctuation.replace(dot,''))
@@ -228,14 +237,15 @@ def apply_name_attribute(h5_object, attribute_name, func=string_to_float,
 
 
 class h5Utils():
-    def __init__(self, file_name_or_object, mode = 'r', deep = 10, 
-            name_criteria=None, object_criteria=None, func=None):
+    def __init__(self, file_name_or_object, *, mode = 'r', deep = 10, 
+            name_criteria=None, object_criteria=None, func=None, **kwargs):
        self.file_name_or_object = file_name_or_object
        self.mode = mode
-       self.name_criteria = name_criteria
-       self.object_criteria = object_criteria
+       self.name_criteria = name_criteria or _default_criteria
+       self.object_criteria = object_criteria or _default_criteria
        self.deep = deep
-       self.func = func
+       self.func = func or _defalut_func 
+       self.kwargs = kwargs
 
     @property
     def file_name_or_object(self):
@@ -266,26 +276,60 @@ class h5Utils():
         else:
             raise NameError("mode must be ['r','r+', 'w', 'w-', 'x', 'a']")
 
-    def set_default(self, mode = 'r', name_criteria=None,
-            object_criteria= None, deep=10, func=None):
-        self.mode = mode
-        self.name_criteria = name_criteria 
-        self.object_criteria = object_criteria 
-        self.deep = deep
-        self.func = None
+    @property
+    def keys(self):
+        return self.set_keys()
 
-    def acces_h5(self):
-        return __acces_h5(self.file_name_or_object, mode=self.mode)
+    def set_default(self):
+        self.mode = 'r'
+        self.name_criteria = _default_criteria
+        self.object_criteria = _default_criteria
+        self.deep = 10
+        self.func = _defalut_func
+        return self
 
-    def select_keys(self):
-        return yield_keys(self.file_name_or_object,
+    def set_kwargs(self, *,mode = None, name_criteria=None,
+            object_criteria= None, deep=None, func=None,**kwargs):
+        self.mode = mode or self.mode
+        self.name_criteria = name_criteria or self.name_criteria
+        self.object_criteria = object_criteria or self.object_criteria
+        self.deep = deep or self.deep
+        self.func = func or self.func
+        self.kwargs = kwargs
+        return self
+
+    def acces_h5(self, *,mode = None):
+        self.mode = mode or self.mode
+        return _acces_h5(self.file_name_or_object, mode=self.mode, **self.kwargs)
+
+    def select_items(self, *,name_criteria=None,
+            object_criteria=None, deep=None,
+            mode=None, func= None):
+        """
+        See yield_items
+        """
+        self.set_kwargs(mode=mode, name_criteria=name_criteria, 
+                object_criteria=object_criteria, deep=deep, func=func)
+        return yield_items(self.file_name_or_object,
                 name_criteria=self.name_criteria,
                 object_criteria=self.object_criteria,
                 deep=self.deep, mode=self.mode, func=self.func)
 
+    def set_keys(self, *,name_criteria=None,
+            object_criteria=None, deep=None,
+            mode=None, func= None, ):
+
+        self.set_kwargs(mode=mode, name_criteria=name_criteria, 
+                object_criteria=object_criteria, deep=deep, func=func)
+        keys = ()
+        for h5_object in self.select_items():
+            keys = keys + (h5_object.name,)
+        return keys 
+
     def apply_keys(self, keys):
-        with acces_h5(self) as h5_object:
+        with self.acces_h5() as h5_object:
             for  key in keys:
                 if self.func:
                     self.func(h5_object.get(keys))
                     yield h5_object.get(key)
+
